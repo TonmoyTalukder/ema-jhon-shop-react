d488fc62dec285fca4838d12b785734e522a0426
Tonmoy Talukder <tonmoytalukder2000@gmail.com>
2021-09-24 03:06:44 +0600

    Footer Add

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>&copy; 2021, All Copyrights reserved by Tonmoy</p>
+        </div>
+    );
+};
+
+export default Footer;
